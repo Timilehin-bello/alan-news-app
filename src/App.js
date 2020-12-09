f0d362219c8356d75885ee961eb808c552fc8001
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-12-09 14:15:17 +0100

    Added some card features using material-ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import React, { useState } from "react";
 import "./App.css";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import { useEffect } from "react";
+import NewsCards from "./component/NewsCards/NewsCards";
 
 const alanKey =
   "f7a2bfa6b7bc2e45c0a4676850307b8d2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  const [newsArticles, setNewsArtcle] = useState([]);
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command }) => {
-        if (command === "testCommand") {
-          alert("This code was executed");
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setNewsArtcle(articles);
         }
       },
     });
@@ -20,6 +23,7 @@ function App() {
   return (
     <div className="App">
       <h1>Alan News Application</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 }
